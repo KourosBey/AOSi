2100838dd467eb49619d7a4a0c4c9936b93d65f2
ebrucngl <ebrucengell@hotmail.com>
2021-04-06 17:00:33 +0300

    Yakın Dövüş
    Yakın dövüş için başlangıç yapıldı.

---
+++ Assets/Scripts/YakinDovus.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YakinDovus : MonoBehaviour
+{
+
+    public Animator animator;
+
+
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            Attack();
+        }
+        
+    }
+    void Attack()
+    {
+        animator.SetTrigger("Attack");
+    }
+
+}
